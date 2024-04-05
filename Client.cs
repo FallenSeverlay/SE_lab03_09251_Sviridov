ceea9fe3ba03cdc307f768daba2d6206a0f980c1
Kamil <kamil198021@mail.ru>
2024-04-05 12:40:21 +0300

    part_1

---
+++ Client.cs
@@ -0,0 +1,151 @@
+﻿using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+
+Client client = new Client();
+await client.CreateClient();
+
+public class ClientData
+{
+    public string? Operation { get; set; }
+    public string? FileName { get; set; }
+    public string? FileContents { get; set; }
+}
+
+
+class Client
+{
+    TcpClient client;
+    StreamReader? Reader;
+    StreamWriter? Writer;
+
+    public async Task CreateClient()
+    {
+        client = new TcpClient();
+        try
+        {
+            client.Connect("127.0.0.1", 8888);
+            Console.WriteLine("Подключен к серверу");
+
+            Reader = new StreamReader(client.GetStream());
+            Writer = new StreamWriter(client.GetStream());
+
+            while (true)
+            {
+                await SendMessageAsync(Writer);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    async Task SendMessageAsync(StreamWriter writer)
+    {
+        try
+        {
+            Console.Write("Введите операцию (1 - получить, 2 - сохранить, 3 - удалить): > ");
+            string? operation = Console.ReadLine();
+
+            if (operation == null || operation == "" || (operation != "1" && operation != "2" && operation != "3")) 
+            { Console.WriteLine("Неправильный ввод"); return; }
+
+            ClientData data = new ClientData();
+
+            string? fileName = "";
+            string? fileContents = "";
+
+            switch (operation)
+            {
+                case "1": // GET
+                    Console.Write("Введите имя файла: > ");
+                    fileName = Console.ReadLine();
+                    if (fileName == null || fileName == "") { Console.WriteLine("Неправильный ввод"); return; }
+
+                    data.Operation = "GET";
+                    data.FileName = fileName;
+
+                    await writer.WriteLineAsync(JsonSerializer.Serialize(data));
+                    await writer.FlushAsync();
+
+                    break;
+
+                case "2": // PUT
+                    Console.Write("Введите имя файла: > ");
+                    fileName = Console.ReadLine();
+                    if (fileName == null || fileName == "") { Console.WriteLine("Неправильный ввод"); return; }
+
+                    Console.Write("Введите контент файла: > ");
+                    fileContents = Console.ReadLine();
+                    if (fileName == null) fileContents = "";
+
+                    data.Operation = "PUT";
+                    data.FileName = fileName;
+                    data.FileContents = fileContents;
+
+                    await writer.WriteLineAsync(JsonSerializer.Serialize(data));
+                    await writer.FlushAsync();
+
+                    break;
+
+                case "3": // DELETE
+                    Console.Write("Введите имя файла: > ");
+                    fileName = Console.ReadLine();
+                    if (fileName == null || fileName == "") { Console.WriteLine("Неправильный ввод"); return; }
+
+                    data.Operation = "DELETE";
+                    data.FileName = fileName;
+
+                    await writer.WriteLineAsync(JsonSerializer.Serialize(data));
+                    await writer.FlushAsync();
+
+                    break;
+            }
+
+            await ReceiveMessageAsync(operation, Reader);
+        }
+        catch
+        {
+            Console.WriteLine("Неправильный ввод");
+            return;
+        }
+    }
+
+    async Task ReceiveMessageAsync(string operation, StreamReader reader)
+    {
+        try
+        {
+            string? message = await reader.ReadLineAsync();
+            if (string.IsNullOrEmpty(message)) return;
+            string code = message.Substring(0, 3);
+
+            switch (operation)
+            {
+                case "1": // GET
+                    if (code == "200") Console.WriteLine($"Сервер нашептывает внутренности: {message.Substring(4, message.Length - 4)}" + " " + (message.Length - 4 == 0 ? "(Их нет)" : ""));
+                    if (code == "404") Console.WriteLine("Сервер нашептывает, что файл не существует");
+
+                    break;
+
+                case "2": // PUT
+                    if (code == "200") Console.WriteLine("Сервер нашептывает, что файл успешно создан");
+                    if (code == "403") Console.WriteLine("Сервер нашептывает, что файл уже был создан");
+
+                    break;
+
+                case "3": // DELETE
+                    if (code == "200") Console.WriteLine("Сервер нашептывает, что файл успешно удален");
+                    if (code == "404") Console.WriteLine("Сервер нашептывает, что файл не существует");
+
+                    break;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Соединение разорвано");
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+Server server = new Server();
+await server.ListenAsync();
+
+public class ClientData
+{
+    public string? Operation { get; set; }
+    public string? FileName { get; set; }
+    public string? FileContents { get; set; }
+}
+
+class Server
+{
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 8888);
+    List<Client> clients = new List<Client>();
+    string path = "C:/Универ/Программная Инженерия/SE_lab03_09251_Sviridov/server/data/";
+    public Dictionary<string, string> serverFiles { get; set; } = new Dictionary<string, string>();
+
+    private void PreloadFiles()
+    {
+        string[] files = Directory.GetFiles(path);
+
+        foreach (string file in files)
+        {
+            string fileName = file.Replace(path, "");
+
+            if (!serverFiles.ContainsKey(fileName)) serverFiles.Add(fileName, "");
+        }
+    }
+
+    public string PutFile(string fileName, string fileContents)
+    {
+        if (!serverFiles.ContainsKey(fileName)) 
+        {
+            File.WriteAllText(path + fileName, fileContents);
+
+            serverFiles.Add(fileName, fileContents);
+            return "200";
+        }
+        else return "403";
+    }
+
+    public string GetFile(string fileName)
+    {
+        if (!serverFiles.ContainsKey(fileName)) return "404";
+        else
+        {
+            return "200" + " " + File.ReadAllText(path + fileName); 
+        }
+    }
+
+    public string DeleteFile(string fileName)
+    {
+        if (!serverFiles.ContainsKey(fileName)) return "404";
+        else
+        {
+            serverFiles.Remove(fileName);
+            File.Delete(path + fileName);
+
+            return "200";
+        }
+    }
+
+    public async Task ListenAsync()
+    {
+        PreloadFiles();
+
+        try
+        {
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                Client client = new Client(tcpClient, this);
+                clients.Add(client);
+
+                Console.WriteLine($"[{DateTime.Now}] Подключился {client.uid}");
+
+                Task.Run(client.ProcessAsync);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+class Client
+{
+    public string uid = Guid.NewGuid().ToString().Substring(0, 8);
+    private TcpClient client;
+    private Server server;
+
+    private StreamReader Reader { get; }
+    private StreamWriter Writer { get; }
+
+    public Client(TcpClient client, Server server)
+    {
+        this.client = client;
+        this.server = server;
+
+        var stream = client.GetStream();
+
+        Reader = new StreamReader(stream);
+        Writer = new StreamWriter(stream);
+    }
+
+    private string PutFile(ClientData clientFile)
+    {
+        return server.PutFile(clientFile.FileName, clientFile.FileContents);
+    }
+
+    private string GetFile(ClientData clientFile)
+    {
+        return server.GetFile(clientFile.FileName);
+    }
+
+    private string DeleteFile(ClientData clientFile)
+    {
+        return server.DeleteFile(clientFile.FileName);
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            string? message;
+            string result;
+
+            while (true)
+            {
+                message = await Reader.ReadLineAsync();
+                if (message == null || message == "") break;
+
+                try
+                {
+                    ClientData? clientData = JsonSerializer.Deserialize<ClientData>(message);
+
+                    switch (clientData.Operation)
+                    {
+                        case "GET":
+                            result = GetFile(clientData);
+
+                            if (result.Split(" ")[0] == "200") Console.WriteLine($"[{DateTime.Now}] {uid} читает файл {clientData.FileName}");
+                            else Console.WriteLine($"[{DateTime.Now}] {uid} пытался читать {clientData.FileName}, но его не существует");
+
+                            await Writer.WriteLineAsync(result);
+                            await Writer.FlushAsync();
+
+                            break;
+
+                        case "PUT":
+                            result = PutFile(clientData);
+
+                            if (result == "200") Console.WriteLine($"[{DateTime.Now}] {uid} создал файл {clientData.FileName}");
+                            else Console.WriteLine($"[{DateTime.Now}] {uid} пытался создать файл {clientData.FileName}, но он уже существует");
+
+                            await Writer.WriteLineAsync(result);
+                            await Writer.FlushAsync();
+
+                            break;
+
+                        case "DELETE":
+                            result = DeleteFile(clientData);
+
+                            if (result == "200") Console.WriteLine($"[{DateTime.Now}] {uid} удалил файл {clientData.FileName}");
+                            else Console.WriteLine($"[{DateTime.Now}] {uid} пытался удалить {clientData.FileName}, но его не существует");
+
+                            await Writer.WriteLineAsync(result);
+                            await Writer.FlushAsync();
+
+                            break;
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"[{DateTime.Now}] {uid} был отключен");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
