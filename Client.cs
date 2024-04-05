166d3fe58d7a713c891384997fbb96ab2b784ccc
Kamil <kamil198021@mail.ru>
2024-04-05 13:02:30 +0300

    part_2

--- Client.cs
+++ Client.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Sockets;
+using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using System.Text.Json;
@@ -10,9 +10,30 @@ await client.CreateClient();
 
 public class ClientData
 {
-    public string? Operation { get; set; }
-    public string? FileName { get; set; }
-    public string? FileContents { get; set; }
+    public string? Operation { get; set; } = "";
+    public string? FileName { get; set; } = "";
+    public string? FileNameServer { get; set; } = "";
+    public string? FileContents { get; set; } = ""; 
+    public bool isByte { get; set; }
+
+    public override string ToString()
+    {
+        string result = "";
+        try
+        {
+            result =
+                $"|{Operation.Length}|" + Operation +
+                $"|{FileName.Length}|" + FileName +
+                $"|{FileNameServer.Length}|" + FileNameServer +
+                $"|{FileContents.Length}|" + FileContents +
+                $"|{(isByte ? 1 : 0)}|" + $"{(isByte ? 1 : 0)}";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        return result;
+    }
 }
 
 
@@ -21,6 +42,10 @@ class Client
     TcpClient client;
     StreamReader? Reader;
     StreamWriter? Writer;
+
+    string[] textFileFormats = { ".txt", ".bin", ".kamil", ".py", ".json" };
+    string[] byteFileFormats = { ".jpg", ".jpeg", ".gif", ".png", ".zip", ".rar", ".7z" };
+    string path = "C:/Универ/Программная Инженерия/SE_lab03_09251_Sviridov/client/data/";
 
     public async Task CreateClient()
     {
@@ -50,57 +75,122 @@ class Client
             Console.Write("Введите операцию (1 - получить, 2 - сохранить, 3 - удалить): > ");
             string? operation = Console.ReadLine();
 
-            if (operation == null || operation == "" || (operation != "1" && operation != "2" && operation != "3")) 
+            if (operation == null || operation == "" || (operation != "1" && operation != "2" && operation != "3" && operation != "exit")) 
             { Console.WriteLine("Неправильный ввод"); return; }
 
             ClientData data = new ClientData();
 
             string? fileName = "";
             string? fileContents = "";
+            string? fileNameServer = "";
+            string? getType = "";
 
             switch (operation)
             {
                 case "1": // GET
-                    Console.Write("Введите имя файла: > ");
+                    Console.Write("Хотите получить по имени или ID (1 - имени, 2 - ID): > ");
+                    getType = Console.ReadLine();
+                    if (getType == null || getType == "" || (getType != "1" && getType != "2")) { Console.WriteLine("Неправильный ввод"); return; }
+
+                    Console.Write($"Введите {(getType == "1" ? "имя" : "ID")} файла: > ");
                     fileName = Console.ReadLine();
                     if (fileName == null || fileName == "") { Console.WriteLine("Неправильный ввод"); return; }
 
-                    data.Operation = "GET";
+                    data.Operation = "GET" + " " + (getType == "1" ? "BY_NAME" : "BY_ID");
                     data.FileName = fileName;
 
-                    await writer.WriteLineAsync(JsonSerializer.Serialize(data));
+                    Console.WriteLine("Запрос отправлен");
+
+                    await writer.WriteLineAsync(data.ToString());
                     await writer.FlushAsync();
 
                     break;
 
                 case "2": // PUT
-                    Console.Write("Введите имя файла: > ");
+                    Console.Write("Введите имя файла клиента: > ");
                     fileName = Console.ReadLine();
                     if (fileName == null || fileName == "") { Console.WriteLine("Неправильный ввод"); return; }
 
-                    Console.Write("Введите контент файла: > ");
-                    fileContents = Console.ReadLine();
-                    if (fileName == null) fileContents = "";
+                    Console.Write("Введите имя файла на сервере: > ");
+                    fileNameServer = Console.ReadLine();
+                    if (fileNameServer == null) { fileNameServer = ""; }
 
-                    data.Operation = "PUT";
-                    data.FileName = fileName;
-                    data.FileContents = fileContents;
+                    if (textFileFormats.Any(fileName.Contains)) // Текстовые Файлы
+                    {
+                        Console.Write("Введите контент файла: > ");
+                        fileContents = Console.ReadLine();
+                        if (fileName == null) fileContents = "";
 
-                    await writer.WriteLineAsync(JsonSerializer.Serialize(data));
-                    await writer.FlushAsync();
+                        data.Operation = "PUT";
+                        data.FileName = fileName;
+                        data.FileNameServer = fileNameServer;
+                        data.FileContents = fileContents;
+                        data.isByte = false;
+
+                        try
+                        {
+                            await writer.WriteLineAsync(data.ToString());
+                            await writer.FlushAsync();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Неправильный ввод {e}");
+                            return;
+                        }
+                    } 
+                    else if (byteFileFormats.Any(fileName.Contains)) // Прочие файлы
+                    {
+                        data.Operation = "PUT";
+                        data.FileName = fileName;
+                        data.FileNameServer = fileNameServer;
+                        data.FileContents = Convert.ToBase64String(File.ReadAllBytes(path + fileName));
+                        data.isByte = true;
+
+                        try
+                        {
+                            await writer.WriteLineAsync(data.ToString());
+                            await writer.FlushAsync();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Неправильный ввод {e}");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неизвестный формат"); return;
+                    }
 
                     break;
 
                 case "3": // DELETE
-                    Console.Write("Введите имя файла: > ");
+                    Console.Write("Хотите удалить по имени или ID (1 - имени, 2 - ID): > ");
+                    getType = Console.ReadLine();
+                    if (getType == null || getType == "" || (getType != "1" && getType != "2")) { Console.WriteLine("Неправильный ввод"); return; }
+
+                    Console.Write($"Введите {(getType == "1" ? "имя" : "ID")} файла: > ");
                     fileName = Console.ReadLine();
                     if (fileName == null || fileName == "") { Console.WriteLine("Неправильный ввод"); return; }
 
-                    data.Operation = "DELETE";
+                    data.Operation = "DELETE" + " " + (getType == "1" ? "BY_NAME" : "BY_ID"); ;
                     data.FileName = fileName;
 
-                    await writer.WriteLineAsync(JsonSerializer.Serialize(data));
+                    await writer.WriteLineAsync(data.ToString());
                     await writer.FlushAsync();
+
+                    break;
+
+                case "exit": // exit
+                    Console.WriteLine("Запрос отправлен");
+
+                    data.Operation = "exit";
+                    await writer.WriteLineAsync(data.ToString());
+                    await writer.FlushAsync();
+
+                    client.Close();
+
+                    Environment.Exit(0);
 
                     break;
             }
@@ -125,13 +215,23 @@ class Client
             switch (operation)
             {
                 case "1": // GET
-                    if (code == "200") Console.WriteLine($"Сервер нашептывает внутренности: {message.Substring(4, message.Length - 4)}" + " " + (message.Length - 4 == 0 ? "(Их нет)" : ""));
+                    if (code == "200")
+                    {
+                        string fileName = "";
+
+                        Console.Write($"Сервер нашептывает внутренности, введите имя файла на клиенте: > ");
+                        fileName = Console.ReadLine();
+                        while (fileName == null || fileName == "") Console.WriteLine("Неправильный ввод");
+
+                        File.WriteAllBytes(path + fileName, Convert.FromBase64String(message.Substring(4, message.Length - 4)));
+                        Console.WriteLine("Файл сохранен на диске!");
+                    }
                     if (code == "404") Console.WriteLine("Сервер нашептывает, что файл не существует");
 
                     break;
 
                 case "2": // PUT
-                    if (code == "200") Console.WriteLine("Сервер нашептывает, что файл успешно создан");
+                    if (code == "200") Console.WriteLine($"Сервер нашептывает, что файл успешно создан и ID = {message.Substring(4, message.Length - 4)}");
                     if (code == "403") Console.WriteLine("Сервер нашептывает, что файл уже был создан");
 
                     break;
